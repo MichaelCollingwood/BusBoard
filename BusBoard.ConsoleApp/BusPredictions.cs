f91efaedf0a142514a5dbd30d36e788a23cddeb7
Michael Collingwood <mike@fairways.me>
2020-07-09 11:07:55 +0100

    Extract Json from api into BusPredictions class

---
+++ BusBoard.ConsoleApp/BusPredictions.cs
@@ -0,0 +1,10 @@
+﻿namespace BusBoard.ConsoleApp
+{
+    public class BusPredictions
+    {
+        public string StationName { get; set; }
+        public string LineName { get; set; }
+        public string TimeToStation { get; set; }
+        public string DestinationName { get; set; }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace BusBoard.ConsoleApp
 {
@@ -10,6 +14,21 @@ namespace BusBoard.ConsoleApp
   {
     static void Main(string[] args)
     {
+      ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+      
+      Console.WriteLine("Give bus-stop code");
+      var busStopCode = Console.ReadLine();
+      
+      BusTimes(busStopCode);
+    }
+
+    static void BusTimes(string busStopCode)
+    {
+      var client = new RestClient("https://api.tfl.gov.uk/StopPoint");
+      var request = new RestRequest("490008660N/Arrivals?app_id=e359379b&app_key=1c36f466bbec601c4e93d8bbfa43525e");
+      var response = client.Execute<List<BusPredictions>>(request).Data;
+      
+      Console.WriteLine(response.First().DestinationName);
     }
   }
 }
